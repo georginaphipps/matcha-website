7deca4053f66124be6382fdd6213b6ed495af858
georginaphipps <146371325+georginaphipps@users.noreply.github.com>
2024-03-27 18:13:33 +0000

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-console.log("hello")
+function buyMatcha() {
+    let name = prompt("What is your name?");
+    let email = prompt("What is your email address?");
+
+    if (name && email) {
+    alert("Thank you " + name + ", please check your email for matcha voucher🍵");
+    } else {
+    alert("Please enter your name and email address to receive matcha voucher!");
+    }
+  }
+  let button = document.querySelector("button");
+  button.addEventListener("click", buyMatcha);
